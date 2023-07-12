8568e1cf0196badf849a03928abdf5598da96acd
Eduardo da Silva <eduardosilva.pn@gmail.com>
2023-07-12 14:14:05 -0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,6 +5,22 @@ lvim.plugins = {
       require("hop").setup({})
     end
   },
+  "wiliamks/nice-reference.nvim",
 }
 
+
 lvim.keys.normal_mode["<Leader>i"] = ":HopChar2<CR>"
+lvim.keys.normal_mode["<Leader>o"] = "<cmd>lua require('nice-reference').references()<CR>"
+
+--local linters = require "lvim.lsp.null-ls.linters"
+--linters.setup {
+--   { command = "eslint", filetypes = { "typescript", "typescriptreact" } }
+-- }
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  {
+    command = "prettier",
+    filetypes = { "typescript", "typescriptreact" },
+  },
+}
